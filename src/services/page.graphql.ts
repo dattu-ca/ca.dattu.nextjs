f05f2d4d81f8bb316ff70d078046305a33b5bb28
Dattu Patel <dattu.patel@gmail.com>
2022-10-31 13:58:23 -0400

    [#183671188] Done getting pages from contentful

---
+++ src/services/page.graphql.ts
@@ -0,0 +1,57 @@
+import {gql} from "@apollo/client";
+import {gqlClient} from "./contentful.config";
+import {iPage, iPages} from "~src/models";
+
+
+const morphPages = (base: any): Array<iPage> => {
+    return base?.data?.pageCollection?.items?.map((item: any) => ({
+        title: item.title,
+        slug: item.slug,
+        content: item.content
+    } as iPage));
+};
+
+const getAllPages = async (): Promise<iPages> => {
+    const result = await gqlClient.query({
+        query: gql`
+        query {
+          pageCollection{
+            items{
+              id
+              title
+              slug
+              content{
+                json
+              }
+            }
+          }
+        }
+    `
+    });
+    const pages = morphPages(result);
+    return {
+        pages
+    };
+};
+
+const getPage = async (slug: string): Promise<iPage> => {
+    const result = await gqlClient.query({
+        query: gql`
+            query {
+              pageCollection (where :{ slug:"${slug}" } ){
+                items{
+                  title
+                  slug
+                  content{
+                    json
+                  }
+                }
+              }
+            }
+        `
+    });
+    const pages = morphPages(result);
+    return pages[0];
+};
+
+export {getAllPages, getPage};
